d8d296482c7fb5ee70745179de589cd748da9dfc
rox <cernatmonicaroxana@gmail.com>
2020-11-07 13:15:31 +0200

    OFB encrytion

--- encryption_modes.py
+++ encryption_modes.py
@@ -5,7 +5,7 @@ from Crypto.Cipher import AES
 ecb_key = urandom(32)
 ofb_key = urandom(32)
 
-iv = urandom(16)
+init_vect = urandom(16)
 
 
 def padding(text):
@@ -21,8 +21,8 @@ def unpadding(text):
 
 
 class ECB:
-    def __init__(self, ecb_key):
-        self.key = ecb_key
+    def __init__(self, key):
+        self.key = key
         self.cipher = AES.new(self.key, AES.MODE_ECB)
 
     def enrypt(self, plain_text):
@@ -40,18 +40,34 @@ class ECB:
 
 
 class OFB:
-    def __init__(self, ofb_key):
-        self.key = ofb_key
-        self.iv = iv
+    def __init__(self, key, iv):
+        self.key = key
         self.cipher = AES.new(self.key, AES.MODE_ECB)
-        self.key_block = self.cipher.encrypt(iv)
+        self.iv = iv
+        self.key_stream = self.cipher.encrypt(self.iv)
+        self.pos = 0
 
+    def encrypt(self, plain_text):
+        plain_text = bytes(plain_text, 'utf-8')
+        cipher_text = b''
 
+        for plain_byte in plain_text:
+            if self.pos < 16:
+                cipher_text += bytes(self.key_stream[self.pos] ^ plain_byte)
+                self.pos += 1
+            else:
+                self.key_stream = self.cipher.encrypt(self.key_stream)
+        return cipher_text
 
 
 
 ecb = ECB(ecb_key)
-plaintext = input("Enter a text to encrypt with Aes in ecb mode: ")
-encrypted = ecb.enrypt(plaintext)
-print("Encrypted: {}".format(encrypted))
-print("Decrypted: {}".format(ecb.decrypt(encrypted)))
+ecb_plaintext = input("Enter a text to encrypt with AES in ECB mode: ")
+ecb_encrypted = ecb.enrypt(ecb_plaintext)
+print("[ECB] Encrypted: {}".format(ecb_encrypted))
+print("[ECB]Decrypted: {}".format(ecb.decrypt(ecb_encrypted)))
+
+ofb = OFB(ofb_key, init_vect)
+ofb_plaintext = input("Enter a text to encrypt with AES in OFB mode: ")
+ofb_encrypted = ofb.encrypt(ofb_plaintext)
+print("[OFB] Encrypted: {}". format(ofb_encrypted))
